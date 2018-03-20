4b8d79e80b7e6d73b1e080160679e7a5c4874d4b
luqieee <36510088+luqieee@users.noreply.github.com>
2018-03-20 11:49:11 +0100

    Using loops

--- test.js
+++ test.js
@@ -1,9 +1,8 @@
 
 var links = document.getElementsByTagName("a");
 
-for (i = 1; i <= links.length; i++){
+for(i = 0; i < links.length; i++){
 
-    console.log("This is link number" + i)
+    links[i].className = "link-" + i;
+
 }
-
-document.write("all links now looped");
