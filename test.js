0b29ca08e9d3afb03fe0e73863d5aec8cc64814f
Łukasz <36510088+luqieee@users.noreply.github.com>
2018-03-22 13:57:11 +0100

    Objects

--- test.js
+++ test.js
@@ -1,25 +1,21 @@
+var myArray = new Array ();
+myArray[0] = 8;
+myArray[1] = "hello";
 
-var myArray = [];
+var myCar = new Object();
+myCar.maxSpeed = 50;
+myCar.driver = "Lucas";
+myCar.drive = function(){console.log("now driving");};
 
-myArray[0] = "I";
-myArray[1] = "have";
-myArray[2] = "no";
-myArray[3] = "questions";
+myCar.drive();
 
-console.log(myArray);
+var myCar2 = {
+    maxSpeed: 70,
+    driver: "Net Ninja",
+    drive: function(speed, time){
+        console.log("Distance traveled " + speed * time);}
 
+};
 
-var myArray2 = [10, 20, "hi", false, 15, "x"];
-console.log(myArray2);
-
-console.log("Number of words in myArray: " + myArray.length);
-console.log("Number of words in myArray2: " + myArray2.length);
-
-
-var myArray3 = ["c", "a", "d", "b"];
-console.log(myArray3);
-console.log("Next array: " + myArray3);
-console.log("Number of words in myArray3 " + myArray3.length);
-console.log("Sorted letters: " + myArray3.sort());
-console.log("Reversed letters: " + myArray3.reverse());
-
+console.log(myCar2.maxSpeed);
+myCar2.drive(50, 3);
