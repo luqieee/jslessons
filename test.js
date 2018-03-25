6d397eedc8b453add905f8a2927cdd80226acdf4
Łukasz <36510088+luqieee@users.noreply.github.com>
2018-03-25 19:04:16 +0200

    Onload event

--- test.js
+++ test.js
@@ -1,18 +1,28 @@
+function setUpEvents(){
 
-var content = document.getElementById("content");
-var button = document.getElementById("show-more");
+    var content = document.getElementById("content");
+    var button = document.getElementById("show-more");
 
-button.onclick = function(){
+    button.onclick = function(){
 
-  if(content.className == "open"){
-      //shrink the box
-      content.className = "";
-      button.innerHTML = "Show More";
+        if(content.className == "open"){
+            //shrink the box
+            content.className = "";
+            button.innerHTML = "Show More";
 
-  }  else{
-      //expand he box
-   content.className = "open";
-   button.innerHTML = "Show Less";
-  }
+        }  else{
+            //expand he box
+            content.className = "open";
+            button.innerHTML = "Show Less";
+        }
+
+    };
+
+}
+
+
+window.onLoad = function(){
+
+    setUpEvents();
 
 };
